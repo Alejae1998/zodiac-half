dc370368229e4c0dd97fa1f3cd27d00c4ed1eee0
Alejae1998 <alejagr1998@gmail.com>
2022-06-06 16:42:57 -0700

    funcionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
-{/* import the zodiac array */}
+{
+  /* import the zodiac array */
+}
+import './Main/Main.css';
+import Header from './Header/Header';
+import Main from './Main/Main';
+import Footer from './Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="zodiac">
+      <Header />
+      <Main />
+      <Footer />
       {/* import and use the Header. Main, and Footer components here */}
       {/* the Header component needs a string passed in as the `name` prop */}
       {/* the Footer component needs a year passed in as the `year` prop */}
